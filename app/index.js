2419feb4901761663c224419e4c044dc708b139b
ryandeussing <ryan@ryandeussing.com>
2017-02-23 15:55:03 -0500

    configuring HMR

--- app/index.js
+++ app/index.js
@@ -1,3 +1,18 @@
 import component from './component';
 
-document.body.appendChild(component());
+let demoComponent = component();
+
+document.body.appendChild(demoComponent);
+
+// HMR interface
+if(module.hot) {
+  // Capture hot update
+  module.hot.accept('./component', () => {
+    const nextComponent = component();
+
+    // Replace old content with the hot loaded one
+    document.body.replaceChild(nextComponent, demoComponent);
+
+    demoComponent = nextComponent;
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -22,9 +24,61 @@ const commonConfig = {
   ],
 };
 
+function productionConfig() {
+  return commonConfig;
+}
+
+function developmentConfig() {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+
+      // Don't refresh if hot loading fails. If you want
+      // refresh behavior, set hot: true instead.
+      hotOnly: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+
+      // Enable error/warning overlay
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ],
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config,
+    {
+      plugins: commonConfig.plugins.concat(config.plugins),
+    }
+  );
+}
+
 
 module.exports = function(env) {
-  console.log('env', env);
-
-  return commonConfig;
+  if (env === 'production') {
+      return productionConfig();
+    }
+    return developmentConfig();
 };
